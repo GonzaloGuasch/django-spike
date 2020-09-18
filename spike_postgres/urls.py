07be0ac8cb51afac445bab7c6d6b293682a70a6d
GonzaloGuasch <gonzaloguasch@outlook.com>
2020-09-18 15:12:29 -0300

    Genero de token para superuser, se requiere para los endpoits

--- spike_postgres/urls.py
+++ spike_postgres/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register('disease', views.DiseaseView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- spike_postgres/views.py
+++ spike_postgres/views.py
@@ -1,8 +1,11 @@
 from .models import Disease
 from rest_framework import viewsets
 from .serializers import DiseaseSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class DiseaseView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+
     queryset = Disease.objects.all()
     serializer_class = DiseaseSerializer
