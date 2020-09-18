46fbb6f4e08e40599a7e904325b6fb9ea8d8f8a3
GonzaloGuasch <gonzaloguasch@outlook.com>
2020-09-18 12:17:49 -0300

    agrego postgres como bd principal

--- spike/settings.py
+++ spike/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
 ]
 
 MIDDLEWARE = [
@@ -79,8 +77,12 @@ WSGI_APPLICATION = 'spike.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_spike',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
